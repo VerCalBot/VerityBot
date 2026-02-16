f30424a0f7169fb3e442c86163075346c2fab265
Jalen Hatcher <jmhatc04@louisville.edu>
2026-02-15 23:58:34 -0500

    yeahhh

---
+++ src/Application.py
@@ -0,0 +1,22 @@
+import logging
+import CLI
+import Utils
+
+from Verkada import VerkadaContext
+from Pipeline import Pipeline
+
+def init():
+    args = CLI.setup_cli()
+    ds = Pipeline()
+    verkada = VerkadaContext(ds, time_delta=1)
+
+    logging.info(f"Initializing Verkada service")
+    verkada.login(args.verkada_api_key)
+
+
+    while verkada.next_page_available():
+        current_page = verkada.get_next_page()
+        if not verkada.EOR_page():
+            # right now, verkada's current pipeline is doing nothing
+            verkada.pipe()
+            Utils.pretty_print_json(current_page['events'][0])
