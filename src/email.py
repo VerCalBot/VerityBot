d140f2ea076fad9be0c6b0bed9dd6cbaf6d26abb
Tyler Sexton <t0sext01@louisville.edu>
2026-02-17 10:35:23 -0500

    Updated using MIMEMultipart to add subject to the Email. Placed a section in the .ini to customize the subject.

--- src/email.py
+++ src/email.py
@@ -2,6 +2,8 @@ import os
 import smtplib
 import configparser
 from dotenv import load_dotenv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 config = configparser.ConfigParser()
 config.read("../config.ini")
@@ -9,7 +11,16 @@ config.read("../config.ini")
 # Assign variables from ini
 recipient_email = config ['Email'] ['EMAIL_TO']
 sender_email = config ['Email'] ['EMAIL_FROM']
-message = config ['Email'] ['EMAIL_MESSAGE']
+email_subject = config ['Email'] ['EMAIL_SUBJECT']
+email_message = config ['Email'] ['EMAIL_MESSAGE']
+
+
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = recipient_email
+message["Subject"] = email_subject
+body = email_message
+message.attach(MIMEText(body, "plain"))
 
 load_dotenv()
 # Retrieve password from env
@@ -24,6 +35,6 @@ s.starttls()
 
 s.login(sender_email, email_password)
 
-s.sendmail(sender_email,recipient_email,message)
+s.sendmail(sender_email,recipient_email,message.as_string())
 
 s.quit()
