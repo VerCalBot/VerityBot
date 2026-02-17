ec42538ff894097afe494ec1d057f253f3877f40
Tyler Sexton <t0sext01@louisville.edu>
2026-02-16 20:08:50 -0500

    Updated the env and ini files to provide all the information needed for SMTP.

--- src/email.py
+++ src/email.py
@@ -1,14 +1,28 @@
+import os
 import smtplib
+import configparser
+# from dotenv import load_dotenv
+#Remove docker got it
+# load_dotenv()
+
+config = configparser.ConfigParser()
+config.read("../config.ini")
+
+#Assign variables from ini
+recipient_email = config ['Email'] ['EMAIL_TO']
+sender_email = config ['Email'] ['EMAIL_FROM']
+message = config ['Email'] ['EMAIL_MESSAGE']
+
+#Assign variable from env
+email_password = os.getenv("EMAIL_PASSWORD")
 
 # creates SMTP session
 s = smtplib.SMTP('smtp.office365.com',587)
 
 s.starttls()
 
-s.login("sender_email","sender_pass")
+s.login(sender_email, email_password)
 
-content = "link_to_Kibana"
-
-s.sendmail("sender_email","receiver_email",content)
+s.sendmail(sender_email,recipient_email,message)
 
 s.quit()
