3d31a8a699da981b6d6b7dcc6db5ba373faad997
Tyler Sexton <t0sext01@louisville.edu>
2026-02-17 09:59:45 -0500

    Added load_dotenv() to fix issue with docker. Had successful test with email to gmail

--- src/email.py
+++ src/email.py
@@ -1,23 +1,24 @@
 import os
 import smtplib
 import configparser
-# from dotenv import load_dotenv
-#Remove docker got it
-# load_dotenv()
+from dotenv import load_dotenv
 
 config = configparser.ConfigParser()
 config.read("../config.ini")
 
-#Assign variables from ini
+# Assign variables from ini
 recipient_email = config ['Email'] ['EMAIL_TO']
 sender_email = config ['Email'] ['EMAIL_FROM']
 message = config ['Email'] ['EMAIL_MESSAGE']
 
-#Assign variable from env
+load_dotenv()
+# Retrieve password from env
 email_password = os.getenv("EMAIL_PASSWORD")
 
 # creates SMTP session
-s = smtplib.SMTP('smtp.office365.com',587)
+s = smtplib.SMTP('smtp.gmail.com',587)
+
+s.set_debuglevel(1)
 
 s.starttls()
 
