d2c9df519e8b070874a406993dfe938b8dfff688
Jalen Hatcher <jmhatc04@louisville.edu>
2026-03-18 10:24:17 -0400

    Iterating through the entirety of a request now

--- src/Application.py
+++ src/Application.py
@@ -1,6 +1,7 @@
 import logging
 import CLI
 import ElasticSearch
+import Utils
 
 from dotenv import load_dotenv
 from Verkada import VerkadaContext
@@ -20,3 +21,10 @@ def init():
                 ElasticSearch.send_bulk_ndjson(verkada.current_page_ndjson_bulk(), password=args.elastic_password)
     else:
         logging.error("ElasticSearch connection timed out")
+
+    # while verkada.next_page_available():
+    #         current_page = verkada.get_next_page()
+    #         if not verkada.is_eor_page():
+    #             # ElasticSearch.send_bulk_ndjson(verkada.current_page_ndjson_bulk(), password=args.elastic_password)
+    #             Utils.pretty_print_json(current_page['events'])
+    #             break
