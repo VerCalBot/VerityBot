a4f1d39fea15fd7148f2bb3c6ccdb3dc1d1631c0
Jalen Hatcher <jmhatc04@louisville.edu>
2026-04-16 17:28:30 -0400

    little hotfix, resolved merge error in application.py

--- src/Application.py
+++ src/Application.py
@@ -1,27 +1,5 @@
 import logging
 import CLI
-<<<<<<< HEAD
-import Utils
-
-from Verkada import VerkadaContext
-from Pipeline import Pipeline
-
-def init():
-    args = CLI.setup_cli()
-    ds = Pipeline()
-    verkada = VerkadaContext(ds, time_delta=1)
-
-    logging.info(f"Initializing Verkada service")
-    verkada.login(args.verkada_api_key)
-
-
-    while verkada.next_page_available():
-        current_page = verkada.get_next_page()
-        if not verkada.EOR_page():
-            # right now, verkada's current pipeline is doing nothing
-            verkada.pipe()
-            Utils.pretty_print_json(current_page['events'][0])
-=======
 import ElasticSearch
 import os
 
@@ -74,4 +52,3 @@ def init():
 
     else:
         logging.error("ElasticSearch connection timed out")
->>>>>>> develop
