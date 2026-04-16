efed9e3a9e2b105be8d4c13261308f31786b43dc
Jalen Hatcher <jmhatc04@louisville.edu>
2026-04-15 23:31:14 -0400

    Implemented functionality to automatically contact the correct smtp server given a sender email in .env

--- src/email_sender.py
+++ src/email_sender.py
@@ -12,21 +12,39 @@ recipient_email = config ['Email'] ['EMAIL_TO']
 sender_email = config ['Email'] ['EMAIL_FROM']
 email_subject = config ['Email'] ['EMAIL_SUBJECT']
 
+domain_to_server_mapping: dict = {
+        "gmail.com": "gmail.com", 
+        "outlook.com": "office365.com",
+        "louisville.edu": "office365.com",
+        "shslou.org": "office365.com"
+}
+
 message = MIMEMultipart()
 message["From"] = sender_email
 message["To"] = recipient_email
 message["Subject"] = email_subject
 
+def domain_to_server(email: str) -> str | None:
+    if '@' in email:
+        domain = email[email.index('@') + 1:]
+        smtp_server = domain_to_server_mapping.get(domain, None)
+        return smtp_server
+    return None
+
 def send_email():
-    try:
-        with smtplib.SMTP("smtp.gmail.com", 587) as s:
-            s.starttls()
-            s.login(sender_email, str(email_password))
-            s.sendmail(sender_email, recipient_email, message.as_string())
-            s.quit()
-    except Exception as e:
-        logging.error(f"Unable to send email: error code {e.args.index}")
-        exit(1)
+    smtp_server = domain_to_server(sender_email)
+    if smtp_server:
+        try:
+            with smtplib.SMTP(f"smtp.{smtp_server}", 587) as s:
+                s.set_debuglevel(1)
+                s.starttls()
+                s.login(sender_email, str(email_password))
+                s.sendmail(sender_email, recipient_email, message.as_string())
+                s.quit()
+        except Exception as e:
+            logging.error(f"Unable to send email: error code {e.args.index}")
+            exit(1)
+    logging.error("Unable to send email. Make sure the sender email is correct!")
 
 # for sending the link to Kibana through email
 # IP address is likely to change due to proxies or DHCP lease termination
@@ -66,3 +84,4 @@ message.attach(MIMEText(html, "html"))
 email_password = os.getenv("EMAIL_PASSWORD")
 
 send_email()
+
